d06f0cde62c21701e9d6400067f88c6e921d982d
suarez9093 <suarez9093@hotmail.com>
2020-10-14 14:42:16 -0400

    added loader

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
+import "./product.css";
 
 function Product() {
   const [product, setProduct] = useState();
-  useEffect(() => {
-    getProduct();
-  }, []);
+  //   useEffect(() => {
+  //     getProduct();
+  //   }, []);
 
   async function getProduct() {
     const url = `http://track.clickbooth.com/i?lid=11851&ua=${navigator.userAgent}&ip=${process.env.REACT_APP_IP}&subid1=&subid2=&subid3=&subid4=&subid5=`;
@@ -12,17 +13,35 @@ function Product() {
     const data = await res.json();
     setProduct(data.data);
   }
-  //   const { click_url, image_url, name, shipping_and_handling } = product;
-  //   console.log(shipping_and_handling);
+  console.log(product);
   return (
-    <div>
-      {!product && <h1>Loading...</h1>}
-      {product && (
+    <>
+      {!product && (
         <>
-          <h1>{product.name}</h1>
+          <h1>Loading...</h1>
+          <div className="lds-dual-ring"></div>
         </>
       )}
-    </div>
+      {product && (
+        <>
+          <div className="product-container">
+            <img
+              className="product-img"
+              src={product.image_url}
+              alt={product.name}
+            />
+            <div className="product-text-container">
+              <h1 className="product-title">{product.name}</h1>
+              <p className="product-price">
+                {product.shipping_and_handling.currency}
+                {product.shipping_and_handling.price}
+              </p>
+              <a href={product.click_url}>Buy now</a>
+            </div>
+          </div>
+        </>
+      )}
+    </>
   );
 }
 
